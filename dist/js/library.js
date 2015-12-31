a8fc80606e316160d485364ccf774deaa604ba09
Andrew Gu <andrew@mingbogu.com>
2015-12-31 09:47:10 -0800

    ajax call done, added in resume

--- dist/js/library.js
+++ dist/js/library.js
@@ -1,8 +1,10 @@
 (function() {
   $(function() {
     return $('#menu a').click(function(e) {
-      e.preventDefault();
-      return $('#content').load($(this).attr('href'));
+      if ($(this).attr('id') !== 'resume') {
+        e.preventDefault();
+        return $('#content').load($(this).attr('href'));
+      }
     });
   });
 
--- src/coffee/library.coffee
+++ src/coffee/library.coffee
@@ -1,5 +1,8 @@
 $ ->
 	$('#menu a').click (e)->
 		# linkID = $(this).attr 'id'
-		e.preventDefault()
-		$('#content').load($(this).attr('href'))
+		# alert $(this).attr('id')
+		if ($(this).attr('id') != 'resume') 
+			e.preventDefault()
+			$('#content').load($(this).attr('href'))
+		
