6fb9e797a4dfe2348c1996b7bf38a609e55e0ea4
Andrew Gu <andrew@mingbogu.com>
2017-06-03 18:12:14 -0500

    update about me

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // A simple server to test this locally
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, () => {
+connect().use(serveStatic(__dirname)).listen(3000, () => {
   console.log('starting on port 8080');
 })
