e08c54b670a5b9bea8372bfa2a118218a0633d99
Andrew Gu <andrew@mingbogu.com>
2018-01-13 02:15:05 -0500

    update description

--- server.js
+++ server.js
@@ -2,5 +2,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(3000, () => {
-  console.log('starting on port 8080');
-})
+  console.log('starting on port 3000');
+});
