28d845f0deb99eeaff7d0c9bb0a4935bfe327cab
Andrew Gu <andrew@mingbogu.com>
2019-11-26 10:06:11 -0800

    webpack for typescript set up

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './ts/library.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader'}
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
