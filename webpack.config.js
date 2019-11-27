d77087ae456246f2e5b706c39c084112f1601bf7
Andrew Gu <andrew@mingbogu.com>
2019-11-26 17:06:24 -0800

    replace scss & ts build scripts with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,40 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './ts/library.ts',
+  entry: ['./ts/library.ts', './scss/library.scss'],
   output: {
-    filename: 'bundle.js',
+    filename: 'library.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
-      { test: /\.ts$/, use: 'ts-loader'}
+      { test: /\.ts$/, use: 'ts-loader' },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader,'css-loader','resolve-url-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: ['.ts', '.js', '.scss', '.css']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'library.css',
+      chunkFilename: '[id].css'
+    })
+  ]
 };
