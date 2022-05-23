b6779e5114f2006ec316d3a4c7656f7f315b2a8e
Andrew Gu <andrew@mingbogu.com>
2022-05-23 08:59:58 -0400

    update resume

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 // Allows me to run a tiny server locally to test
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-import * as nodeStatic from 'node-static';
-import path from 'path';
+import { createServer, IncomingMessage, ServerResponse } from "http";
+import * as nodeStatic from "node-static";
+import path from "path";
 
 const files = new nodeStatic.Server(path.join(__dirname));
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    req.addListener('end', () => {
-        files.serve(req, res);
-    }).resume();
+  req
+    .addListener("end", () => {
+      files.serve(req, res);
+    })
+    .resume();
 });
 
 server.listen(3000, () => {
-    console.log("Listening on port 2999");
+  const x = "blah";
+  console.log("Listening on port 3000");
 });
--- ts/library.ts
+++ ts/library.ts
@@ -1,44 +1,43 @@
-const getHtmlContents = async (name: string = 'home') => {
+const getHtmlContents = async (name: string = "home") => {
   try {
     const fileContents = await fetch(`./dist/${name}.html`);
     return fileContents.text();
   } catch (err) {
     console.log(err);
-    return '<p>The page you\'re trying to access doesn\'t exist!</p>';
+    return "<p>The page you're trying to access doesn't exist!</p>";
   }
 };
 
 const globalLoadingElement = (): HTMLElement => {
-  const loading = document.createElement('div');
-  loading.setAttribute('class', 'mdl-spinner');
-  loading.setAttribute('class', 'mdl-js-spinner');
-  loading.setAttribute('class', 'is-active');
+  const loading = document.createElement("div");
+  loading.setAttribute("class", "mdl-spinner");
+  loading.setAttribute("class", "mdl-js-spinner");
+  loading.setAttribute("class", "is-active");
 
   return loading;
-}
+};
 
 // If the page doesn't exist, then we return 404
 const filterPageValues = (page?: string): string => {
-  if (!page || page === '') page = 'home';
+  if (!page || page === "") page = "home";
 
-  const pagesBank = ['home', 'about'];
-  return (pagesBank.includes(page)) ? page : '404';
-}
+  const pagesBank = ["home", "about"];
+  return pagesBank.includes(page) ? page : "404";
+};
 
 // Get the hash portion of the URL
 const getHashValue = (link: string): string => {
   const hashRegex = /#(\w+)$/;
   return link.match(hashRegex) ? link.match(hashRegex)[1] : undefined;
-}
-
+};
 
 // listeners for the header links
-const navElements = document.getElementsByClassName('site-navigation');
+const navElements = document.getElementsByClassName("site-navigation");
 Array.from(navElements).map((nav: HTMLElement) => {
-  nav.addEventListener('click', async (e: MouseEvent) => {
+  nav.addEventListener("click", async (e: MouseEvent) => {
     e.preventDefault();
-    const bodyContent = document.getElementById('page-content');
-    bodyContent.innerHTML = '';
+    const bodyContent = document.getElementById("page-content");
+    bodyContent.innerHTML = "";
     bodyContent.appendChild(globalLoadingElement());
 
     const link = e.target.toString();
@@ -48,14 +47,18 @@ Array.from(navElements).map((nav: HTMLElement) => {
     const fileContents = await getHtmlContents(hrefValue);
     bodyContent.innerHTML = fileContents;
     if (history)
-      history.pushState({html: fileContents, set: true}, hrefValue, `#${hrefValue ? hrefValue : ''}`);
+      history.pushState(
+        { html: fileContents, set: true },
+        hrefValue,
+        `#${hrefValue ? hrefValue : ""}`
+      );
   });
 });
 
 // On page load, check the URL to see what page to load
-document.addEventListener('DOMContentLoaded', async (e) => {
-  const bodyContent = document.getElementById('page-content');
-  bodyContent.innerHTML = '';
+document.addEventListener("DOMContentLoaded", async (e) => {
+  const bodyContent = document.getElementById("page-content");
+  bodyContent.innerHTML = "";
   bodyContent.appendChild(globalLoadingElement());
 
   // @ts-ignore
@@ -64,20 +67,24 @@ document.addEventListener('DOMContentLoaded', async (e) => {
   let hrefValue = getHashValue(link);
   hrefValue = filterPageValues(hrefValue);
   const fileContents = await getHtmlContents(hrefValue);
-  document.getElementById('page-content').innerHTML = fileContents;
+  document.getElementById("page-content").innerHTML = fileContents;
   // in case someone enters in a wrong url, we want to show them the 404
   if (history)
-    history.replaceState({html: fileContents, set: true}, hrefValue, `#${hrefValue ? hrefValue : ''}`);
+    history.replaceState(
+      { html: fileContents, set: true },
+      hrefValue,
+      `#${hrefValue ? hrefValue : ""}`
+    );
 });
 
 // manually replace page contents if someone uses the browser back/forth buttons
-window.addEventListener('hashchange', async (e) => {
+window.addEventListener("hashchange", async (e) => {
   const oldHash = filterPageValues(getHashValue(e.oldURL));
   const newHash = filterPageValues(getHashValue(e.newURL));
 
   if (oldHash !== newHash) {
-    const bodyContent = document.getElementById('page-content');
-    bodyContent.innerHTML = '';
+    const bodyContent = document.getElementById("page-content");
+    bodyContent.innerHTML = "";
     bodyContent.appendChild(globalLoadingElement());
 
     const fileContents = await getHtmlContents(newHash);
